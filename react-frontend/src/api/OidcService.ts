2a842c90d3a00b666c28800d5052c8a8ec9c2538
Marko Niemi <marko.j.niemi@gmail.com>
2024-06-22 19:31:01 +0300

    localStorage -> sessionStorage, access_token -> id_token; first working version.

--- react-frontend/src/api/OidcService.ts
+++ react-frontend/src/api/OidcService.ts
@@ -2,12 +2,16 @@ import { User } from "oidc-client-ts";
 
 export default class OidcService {
     public static getUser() {
-        const oidcStorage = localStorage.getItem(`oidc.user:<your authority>:<your client id>`);
+        // const oidcStorage = sessionStorage.getItem(`oidc.user:<your authority>:<your client id>`);
+        const oidcStorage = sessionStorage.getItem(
+            `oidc.user:https://accounts.google.com:977467276012-1pkp49dcqgj8k0f3dprbl53avt8sgcep.apps.googleusercontent.com`,
+        );
         if (!oidcStorage) {
             return null;
         }
         return User.fromStorageString(oidcStorage);
     }
+
     public static getHeaders(): Headers {
         const headers = new Headers({ "content-type": "application/json" });
         const token = OidcService.getToken();
@@ -17,8 +21,9 @@ export default class OidcService {
         }
         return headers;
     }
+
     public static getToken(): string {
         const user = OidcService.getUser();
-        return user?.access_token;
+        return user?.id_token;
     }
 }
