940cb7d68c968e2fa011066605941bb0e71b569d
Marko Niemi <marko.j.niemi@gmail.com>
2025-12-20 19:33:58 +0200

    Fix process.env support in vite configs.

--- react-frontend/vite.config.ts
+++ react-frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import {defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
@@ -15,6 +15,9 @@ export default defineConfig(({ mode }) => {
             outDir: "build",
             minify: false,
             sourcemap: true,
+        },
+        define: {
+            'process.env': env,
         },
     };
 });
--- react-frontend/vitest.config.ts
+++ react-frontend/vitest.config.ts
@@ -1,15 +1,22 @@
 import {defineConfig} from 'vitest/config'
 import react from "@vitejs/plugin-react";
+import {loadEnv} from "vite";
 
-export default defineConfig({
-    plugins: [react()],
-    test: {
-        globals: true,
-        environment: "jsdom",
-        include: ["test/**/*-test.ts?(x)"],
-        coverage: {
-            reportsDirectory: "reports/coverage",
-            include: ["test/**", "src/**"]
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        plugins: [react()],
+        test: {
+            globals: true,
+            environment: "jsdom",
+            include: ["test/**/*-test.ts?(x)"],
+            coverage: {
+                reportsDirectory: "reports/coverage",
+                include: ["test/**", "src/**"]
+            },
         },
-    },
+        define: {
+            'process.env': env,
+        },
+    };
 });
