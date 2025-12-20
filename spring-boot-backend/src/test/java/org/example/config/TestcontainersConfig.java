c8d32c8fc210126f5e8046389c0185e019218f71
Marko Niemi <marko.j.niemi@gmail.com>
2025-12-20 17:21:12 +0200

    Use slf4jlogconsumer in Testcontainers and set keycloak log level.

--- spring-boot-backend/src/test/java/org/example/config/TestcontainersConfig.java
+++ spring-boot-backend/src/test/java/org/example/config/TestcontainersConfig.java
@@ -3,6 +3,7 @@ package org.example.config;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 
 import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.HostConfig;
@@ -24,6 +25,12 @@ public class TestcontainersConfig {
             .withAdminPassword("admin")
             .withRealmImportFile("/realm-export.json")
             .withExposedPorts(8080, 9000)
+            //          .withDebug()
+            //          .withDebugFixedPort(8787, false)
+            .withEnv(
+                "KC_LOG_LEVEL",
+                "INFO,demo.keycloak.userstorage.ldap.attributeimport:DEBUG,org.example.keycloak:DEBUG")
+            .withLogConsumer(new Slf4jLogConsumer(log))
             .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(createPortBindings()));
     keycloak.start();
     return keycloak;
