1fb5b3d272b56aa07a93c1d33847709769cd751a
Marko Niemi <marko.j.niemi@gmail.com>
2025-12-20 17:21:51 +0200

    Fix application class name.

--- spring-boot-backend/src/main/java/org/example/OidcDemoApplication.java
+++ spring-boot-backend/src/main/java/org/example/OidcDemoApplication.java
@@ -1,12 +1,11 @@
 package org.example;
 
-import org.example.config.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OidcDemoApplication {
   public static void main(String[] args) {
-    SpringApplication.run(ApplicationConfig.class, args);
+    SpringApplication.run(OidcDemoApplication.class, args);
   }
 }
