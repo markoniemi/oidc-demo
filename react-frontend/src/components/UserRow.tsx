23f4e34a4ed12d3571e056678288f23098a66284
Marko Niemi <marko.j.niemi@gmail.com>
2025-12-27 08:41:58 +0200

    Change UserRow to functional component.

--- react-frontend/src/components/UserRow.tsx
+++ react-frontend/src/components/UserRow.tsx
@@ -1,58 +1,46 @@
-import * as React from "react";
 import User from "../domain/User";
-import {NavLink} from "react-router";
-import {Button} from "react-bootstrap";
-import {FormattedMessage} from "react-intl";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { type NavigateFunction, NavLink, useNavigate } from "react-router";
+import { Button } from "react-bootstrap";
+import { FormattedMessage } from "react-intl";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as Icons from "@fortawesome/free-solid-svg-icons";
-import type {Empty} from "../domain/Empty";
-import withRouter, {type WithRouter} from "./withRouter";
 
-export interface UserProps extends WithRouter {
+export interface UserProps {
     user: User;
-
     deleteUser(id: number | undefined): void;
 }
 
-class UserRow extends React.Component<UserProps, Empty> {
-    constructor(props: UserProps) {
-        super(props);
-        this.delete = this.delete.bind(this);
-        this.editUser = this.editUser.bind(this);
-    }
+export default function UserRow(props: UserProps) {
+    const navigate: NavigateFunction = useNavigate();
+    const user: User = props.user;
 
-    public delete(): void {
-        const {id} = this.props.user;
-        this.props.deleteUser(id);
-    }
+    const deleteUser = (): void => {
+        const { id } = user;
+        props.deleteUser(id);
+    };
 
-    public editUser(): void {
-        const {id} = this.props.user;
-        this.props.router.navigate("/users/" + id);
-    }
+    const editUser = (): void => {
+        const { id } = user;
+        navigate("/users/" + id);
+    };
 
-    public override render(): React.ReactNode {
-        const user: User = this.props.user;
-        return (
-            <tr id={user.username} key={this.props.user.id}>
-                <td id="username">
-                    <NavLink to={"/users/" + user.id}>{user.username}</NavLink>
-                </td>
-                <td id="email">{user.email}</td>
-                <td id="role">
-                    <FormattedMessage id={"role." + user.role}/>
-                </td>
-                <td>
-                    <Button id={`edit.${user.username}`} size="sm" onClick={this.editUser}>
-                        <FontAwesomeIcon icon={Icons.faEdit}/>
-                    </Button>
-                    <Button id={`delete.${user.username}`} size="sm" onClick={this.delete}>
-                        <FontAwesomeIcon icon={Icons.faTrashAlt}/>
-                    </Button>
-                </td>
-            </tr>
-        );
-    }
+    return (
+        <tr id={user.username} key={user.id}>
+            <td id="username">
+                <NavLink to={"/users/" + user.id}>{user.username}</NavLink>
+            </td>
+            <td id="email">{user.email}</td>
+            <td id="role">
+                <FormattedMessage id={"role." + user.role} />
+            </td>
+            <td>
+                <Button id={`edit.${user.username}`} size="sm" onClick={editUser}>
+                    <FontAwesomeIcon icon={Icons.faEdit} />
+                </Button>
+                <Button id={`delete.${user.username}`} size="sm" onClick={deleteUser}>
+                    <FontAwesomeIcon icon={Icons.faTrashAlt} />
+                </Button>
+            </td>
+        </tr>
+    );
 }
-
-export default withRouter(UserRow);
