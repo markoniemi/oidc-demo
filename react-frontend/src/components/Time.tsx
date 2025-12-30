48676d757d052eb11e87b9a251450e5beb3d8fe0
Marko Niemi <ext-marko.niemi@tulli.fi>
2025-12-30 20:37:46 +0200

    Convert TimeService to function.

--- react-frontend/src/components/Time.tsx
+++ react-frontend/src/components/Time.tsx
@@ -1,17 +1,16 @@
 import * as React from "react";
 import { useEffect } from "react";
-import TimeServiceImpl from "../api/TimeServiceImpl";
 import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as Icons from "@fortawesome/free-solid-svg-icons";
 import { useIsMounted } from "usehooks-ts";
+import getTime from "../api/TimeService.ts";
 
 export default function Time() {
-    const helloService = new TimeServiceImpl();
     const [message, setMessage] = React.useState<string>("");
     const isMounted = useIsMounted();
     const fetchMessage = async () => {
-        setMessage(await helloService.getTime());
+        setMessage(await getTime());
     };
     useEffect(() => {
         fetchMessage();
