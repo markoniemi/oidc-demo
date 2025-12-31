a4daf4632306fecb8c4fb59a440b4c7e1d9f4107
Marko Niemi <ext-marko.niemi@tulli.fi>
2025-12-31 16:05:26 +0200

    Revert "Revert "Convert TimeService to function.""
    This reverts commit 295c9b3a5c6ec3b09338b20a4d63b9135fc46fc9.

--- react-frontend/src/api/TimeService.ts
+++ react-frontend/src/api/TimeService.ts
@@ -1,3 +1,14 @@
-export interface TimeService {
-    getTime(): Promise<string>;
+import Http from "./Http";
+
+export default async function getTime(): Promise<string> {
+    const response: Response = await Http.post(getApiUrl(), "world");
+    if (response.ok) {
+        return response.text();
+    } else {
+        throw new Error("error.time");
+    }
+}
+
+function getApiUrl(): string {
+    return "/api/rest/time";
 }
