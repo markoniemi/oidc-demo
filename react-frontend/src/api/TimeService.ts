791480f31835633fcf769ebc590e4c5dab77ee47
Marko Niemi <ext-marko.niemi@tulli.fi>
2025-12-31 16:22:09 +0200

    Revert "Revert "Revert "Convert TimeService to function."""
    This reverts commit a4daf4632306fecb8c4fb59a440b4c7e1d9f4107.

--- react-frontend/src/api/TimeService.ts
+++ react-frontend/src/api/TimeService.ts
@@ -1,14 +1,3 @@
-import Http from "./Http";
-
-export default async function getTime(): Promise<string> {
-    const response: Response = await Http.post(getApiUrl(), "world");
-    if (response.ok) {
-        return response.text();
-    } else {
-        throw new Error("error.time");
-    }
-}
-
-function getApiUrl(): string {
-    return "/api/rest/time";
+export interface TimeService {
+    getTime(): Promise<string>;
 }
--- react-frontend/src/components/Time.tsx
+++ react-frontend/src/components/Time.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 import { useEffect } from "react";
+import TimeServiceImpl from "../api/TimeServiceImpl";
 import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as Icons from "@fortawesome/free-solid-svg-icons";
 import { useIsMounted } from "usehooks-ts";
-import getTime from "../api/TimeService.ts";
 
 export default function Time() {
+    const helloService = new TimeServiceImpl();
     const [message, setMessage] = React.useState<string>("");
     const isMounted = useIsMounted();
     const fetchMessage = async () => {
-        setMessage(await getTime());
+        setMessage(await helloService.getTime());
     };
     useEffect(() => {
         fetchMessage();
