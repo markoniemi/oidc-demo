ad067d351888273737e326b690c83c531ffd8e67
Marko Niemi <marko.j.niemi@gmail.com>
2026-01-06 16:01:04 +0200

    Time uses useQuery.

--- react-frontend/src/components/Time.tsx
+++ react-frontend/src/components/Time.tsx
@@ -1,27 +1,23 @@
-import * as React from "react";
-import { useEffect } from "react";
 import TimeServiceImpl from "../api/TimeServiceImpl";
 import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as Icons from "@fortawesome/free-solid-svg-icons";
-import { useIsMounted } from "usehooks-ts";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Time() {
-    const helloService = new TimeServiceImpl();
-    const [message, setMessage] = React.useState<string>("");
-    const isMounted = useIsMounted();
-    const fetchMessage = async () => {
-        setMessage(await helloService.getTime());
-    };
-    useEffect(() => {
-        fetchMessage();
-    }, [isMounted]);
-    return (
-        <>
-            <Button id="fetchMessage" size="sm" onClick={fetchMessage}>
-                <FontAwesomeIcon icon={Icons.faClock} />
-            </Button>
-            <span id="message">{message}</span>
-        </>
-    );
+  const helloService = new TimeServiceImpl();
+  const { data: message, refetch } = useQuery({
+    queryKey: ["time"],
+    queryFn: () => helloService.getTime(),
+    retry: false,
+  });
+
+  return (
+    <>
+      <Button id="fetchMessage" size="sm" onClick={() => refetch()}>
+        <FontAwesomeIcon icon={Icons.faClock} />
+      </Button>
+      <span id="message">{message}</span>
+    </>
+  );
 }
--- react-frontend/test/components/Time-test.tsx
+++ react-frontend/test/components/Time-test.tsx
@@ -1,40 +1,52 @@
 import * as dotenv from "dotenv";
 import fetchMock from "fetch-mock";
-import {sleep} from "../time";
+import { sleep } from "../time";
 import Time from "../../src/components/Time";
-import {act, configure, fireEvent, render, screen} from "@testing-library/react";
+import { act, configure, fireEvent, render, screen } from "@testing-library/react";
 import AbstractPage from "../pages/AbstractPage";
-import {afterEach, assert, beforeEach, describe, test} from "vitest";
+import { afterEach, assert, beforeEach, describe, test } from "vitest";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const renderTime = async () => {
+  const queryClient = new QueryClient();
+  await render(
+    <QueryClientProvider client={queryClient}>
+      <Time />
+    </QueryClientProvider>,
+  );
+};
 
 describe("Time component", () => {
-    beforeEach(() => {
-        configure({testIdAttribute: "id"});
-        dotenv.config({path: ".env"});
-        fetchMock.mockGlobal();
+  beforeEach(() => {
+    configure({ testIdAttribute: "id" });
+    dotenv.config({ path: ".env" });
+    fetchMock.mockGlobal();
+  });
+  afterEach(() => {
+    fetchMock.hardReset();
+  });
+  test("renders text", async () => {
+    fetchMock.post("/api/rest/time", "response");
+    await act(async () => {
+      renderTime();
     });
-    afterEach(() => {
-        fetchMock.hardReset();
+    await sleep(100);
+    assert.equal((await screen.findByTestId("message")).textContent, "response");
+  });
+  test("updates text after button press", async () => {
+    fetchMock.postOnce("/api/rest/time", "response1");
+    fetchMock.postOnce("/api/rest/time", "response2");
+    await act(async () => {
+      renderTime();
     });
-    test("renders text", async () => {
-        fetchMock.post("/api/rest/time", "response");
-        await act(async () => {
-            await render(<Time/>);
-        });
-        await sleep(100);
-        assert.equal((await screen.findByTestId("message")).textContent, "response");
+      await sleep(500);
+      assert.equal((await screen.findByTestId("message")).textContent, "response1");
+    await act(async () => {
+      await fireEvent.click(await AbstractPage.findButton("fetchMessage"));
     });
-    test("updates text after button press", async () => {
-        fetchMock.postOnce("/api/rest/time", "response1");
-        fetchMock.postOnce("/api/rest/time", "response2");
-        await act(async () => {
-            await render(<Time/>);
-        });
-        await sleep(100);
-        assert.equal((await screen.findByTestId("message")).textContent, "response1");
-        await act(async () => {
-            await fireEvent.click(await AbstractPage.findButton("fetchMessage"));
-            await sleep(100);
-        });
-        assert.equal((await screen.findByTestId("message")).textContent, "response2");
+      await sleep(500);
+    await act(async () => {
+      assert.equal((await screen.findByTestId("message")).textContent, "response2");
     });
+  });
 });
