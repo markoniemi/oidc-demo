0f810fac14ea6c7f68ba25d3b8252dc0f3993895
Marko Niemi <marko.j.niemi@gmail.com>
2026-01-30 20:49:23 +0200

    Remove unused Spring Cloud and Feign dependencies from pom.xml and IntegrationTestConfig

--- spring-boot-backend/src/test/java/org/example/config/IntegrationTestConfig.java
+++ spring-boot-backend/src/test/java/org/example/config/IntegrationTestConfig.java
@@ -4,15 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 
-import feign.RequestInterceptor;
-
 @Import({SeleniumConfig.class})
 public class IntegrationTestConfig {
-
-  @Bean
-  public RequestInterceptor jwtRequestInterceptor() {
-    return new JwtRequestInterceptor();
-  }
 
   @Bean
   public ClientHttpRequestInterceptor restRequestInterceptor() {
